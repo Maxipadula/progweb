033ea0827b5a691f8a935b3952dc3fd597c79e73
Maxipadula <padula.maximiliano@gmail.com>
2015-11-21 19:34:52 -0300

    actualizacion

--- TP FINAL/Web/php/ADMINISTRADOR/GESTION/USUARIOS/ingresar_modificaciones_usuario.php
+++ TP FINAL/Web/php/ADMINISTRADOR/GESTION/USUARIOS/ingresar_modificaciones_usuario.php
@@ -60,5 +60,4 @@
 	}
 	
 	echo("MODIFICACION EXITOSA");
-
 ?>
--- TP FINAL/Web/php/SUPERVISOR/qr.php
+++ TP FINAL/Web/php/SUPERVISOR/qr.php
@@ -1,9 +1,7 @@
 <?php
-	
+	$size = 10;
 	include ('../../phpqrcode/qrlib.php');
-	$size = 5;
 	
-	$param = $_GET['id'];
-	$codeText = 'La ID de tu viaje es: '.$param;
-    QRcode::png($codeText);
+	QRcode::png('PHP QR Code',$size);
+	
 ?>
